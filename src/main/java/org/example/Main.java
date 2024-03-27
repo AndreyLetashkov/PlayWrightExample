4af620e8a947b772db88e96bbd148063bd0ca8fd
andrei <andrei.letashkou@sap.com>
2024-03-27 12:35:42 +0100

    update_27_03

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import com.microsoft.playwright.*;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class Main {
+    public static void main(String[] args) {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(false)
+        );
+        Page page = browser.newPage();
+        page.navigate("https://ecommerce-playground.lambdatest.io/index.php");
+
+        Locator myAccount = page.locator("//a[contains(.,'My account')][@role='button']");
+        myAccount.hover();
+		page.click("//a[contains(.,'Login')]");
+        assertThat(page).hasTitle("Account Login");
+        page.getByPlaceholder("E-Mail Address").type("letashkaf123@gmail.com");
+        page.getByPlaceholder("Password").type("Qazwsx12");
+        page.click("//input[@value='Login']");
+        assertThat(page).hasTitle("My Account");
+        page.close();
+        browser.close();
+        playwright.close();
+    }
+}
